d96eee383276e67be4bd78104ca9c97e46d5922b
jeffreymkabot <jeffrey.m.kabot@gmail.com>
2015-04-12 17:36:04 -0400

    Cleaned up admin class
    removed @override flag

--- ShieldSchedule-Server/src/main/java/sss/entities/Administrator.java
+++ ShieldSchedule-Server/src/main/java/sss/entities/Administrator.java
@@ -28,13 +28,11 @@ public class Administrator extends GenericUser implements Serializable
     protected Administrator(){}
     
 
-    @Override
     public Long getId()
     {
         return id;
     }
 
-    @Override
     public void setId(Long id)
     {
         this.id = id;
