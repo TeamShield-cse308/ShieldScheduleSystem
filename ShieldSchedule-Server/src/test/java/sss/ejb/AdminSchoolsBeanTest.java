6616091f8ec5a3eeea1ab4449902f472aae109f4
weize lin <weizelin925@hotmail.com>
2015-04-14 13:54:35 -0400

    Try a junit test for 2 beans
    It shows the Null pointers exception. can you help check the codes?

---
+++ ShieldSchedule-Server/src/test/java/sss/ejb/AdminSchoolsBeanTest.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sss.ejb;
+
+import java.util.List;
+import javax.ejb.embeddable.EJBContainer;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+import sss.entities.School;
+
+/**
+ *
+ * @author Weize
+ */
+public class AdminSchoolsBeanTest {
+    
+        //reference to the perisstence layer
+    @PersistenceContext
+    private EntityManager em;
+    
+    private AdminSchoolsBean asb;
+    
+    private School sch;
+    
+    private String initName = "Stony Brook";
+    private int initSemesters = 1;
+    private int initPeriods = 2;
+    private int initScheduleDays = 3;
+    private int initStartLunchPeriod = 4;
+    private int initEndLunchPeriod = 5;
+    
+    
+    public AdminSchoolsBeanTest() {
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        asb=new AdminSchoolsBean();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addSchool method, of class AdminSchoolsBean.
+     */
+    @org.junit.Test
+    public void testAddSchool() throws NullPointerException {
+        System.out.println("addSchool");
+        asb.addSchool(initName, initSemesters, initPeriods, initScheduleDays,
+        initStartLunchPeriod, initEndLunchPeriod);
+        sch=em.find(School.class, initName);
+        assertEquals(initName,sch.getName());
+        assertEquals(initSemesters,sch.getNumSemesters());
+        assertEquals(initPeriods,sch.getNumPeriods());
+        assertEquals(initScheduleDays,sch.getNumScheduleDays());
+        assertEquals(initStartLunchPeriod,sch.getStartingLunchPeriod());
+        assertEquals(initEndLunchPeriod,sch.getEndingLunchPeriod());
+        
+        return;
+    }
+
+    /**
+     * Test of deleteSchool method, of class AdminSchoolsBean.
+     */
+    @org.junit.Test
+    public void testDeleteSchool() throws NullPointerException {
+        System.out.println("deleteSchool");
+       asb.deleteSchool(initName);
+       sch= em.find(School.class, initName);
+       if(sch==null)
+           System.out.println("successful deleted");
+        return;
+    }
+
+    /**
+     * Test of editSchool method, of class AdminSchoolsBean.
+      */
+    @org.junit.Test
+    public void testEditSchool() throws NullPointerException {
+           System.out.println("editSchool");
+           asb.editSchool(initName, initName, initSemesters, initPeriods, 
+                   initScheduleDays, initStartLunchPeriod, initEndLunchPeriod);
+                   sch=em.find(School.class, initName);
+        assertEquals(initName,sch.getName());
+        assertEquals(initSemesters,sch.getNumSemesters());
+        assertEquals(initPeriods,sch.getNumPeriods());
+        assertEquals(initScheduleDays,sch.getNumScheduleDays());
+        assertEquals(initStartLunchPeriod,sch.getStartingLunchPeriod());
+        assertEquals(initEndLunchPeriod,sch.getEndingLunchPeriod());
+        
+        return;
+    }
+
+    /**
+     * Test of getAllSchools method, of class AdminSchoolsBean.
+      */
+    @org.junit.Test
+    public void testGetAllSchools() throws NullPointerException {
+        System.out.println("getAllSchools");
+       List<School> schList= asb.getAllSchools();
+       for(int n=0;n<schList.size();n++)
+           System.out.println(schList.get(n).toString());
+       return;
+      
+    }
+}
---
+++ ShieldSchedule-Server/src/test/java/sss/ejb/AdminStudentsBeanTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sss.ejb;
+
+import java.util.List;
+import javax.ejb.embeddable.EJBContainer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sss.entities.Student;
+
+/**
+ *
+ * @author Weize
+ */
+public class AdminStudentsBeanTest {
+    
+    public AdminStudentsBeanTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getAllStudents method, of class AdminStudentsBean.
+     */
+    @Test
+    public void testGetAllStudents() throws Exception {
+        System.out.println("getAllStudents");
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        AdminStudentsBean instance = (AdminStudentsBean)container.getContext().lookup("java:global/classes/AdminStudentsBean");
+        List<Student> expResult = null;
+        List<Student> result = instance.getAllStudents();
+        assertEquals(expResult, result);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPendingStudents method, of class AdminStudentsBean.
+     */
+    @Test
+    public void testGetPendingStudents() throws Exception {
+        System.out.println("getPendingStudents");
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        AdminStudentsBean instance = (AdminStudentsBean)container.getContext().lookup("java:global/classes/AdminStudentsBean");
+        List<Student> expResult = null;
+        List<Student> result = instance.getPendingStudents();
+        assertEquals(expResult, result);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of approveStudent method, of class AdminStudentsBean.
+     */
+    @Test
+    public void testApproveStudent() throws Exception {
+        System.out.println("approveStudent");
+        String email = "";
+        boolean approved = false;
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        AdminStudentsBean instance = (AdminStudentsBean)container.getContext().lookup("java:global/classes/AdminStudentsBean");
+        instance.approveStudent(email, approved);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteStudent method, of class AdminStudentsBean.
+     */
+    @Test
+    public void testDeleteStudent() throws Exception {
+        System.out.println("deleteStudent");
+        String email = "";
+        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        AdminStudentsBean instance = (AdminStudentsBean)container.getContext().lookup("java:global/classes/AdminStudentsBean");
+        instance.deleteStudent(email);
+        container.close();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
