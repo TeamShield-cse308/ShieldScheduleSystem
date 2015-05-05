8f5daf0f1642284920b0a2b85f1a230e401350df
gubyy <evanguby@Evans-MacBook-Pro.local>
2015-05-04 23:16:10 -0400

    generate schedule stuff

---
+++ ShieldSchedule-Client/src/shield/client/view/ViewGeneratedSchedulePageController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shield.client.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author evanguby
+ */
+public class ViewGeneratedSchedulePageController implements Initializable {
+    @FXML
+    private ListView<?> day1Table;
+    @FXML
+    private ListView<?> day2Table;
+    @FXML
+    private ListView<?> day3Table;
+    @FXML
+    private ListView<?> day4Table;
+    @FXML
+    private ListView<?> day5Table;
+    @FXML
+    private ListView<?> day6Table;
+    @FXML
+    private ListView<?> day7Table;
+    @FXML
+    private Label period1;
+    @FXML
+    private Label period2;
+    @FXML
+    private Label period3;
+    @FXML
+    private Label period4;
+    @FXML
+    private Label period5;
+    @FXML
+    private Label period6;
+    @FXML
+    private Label period7;
+    @FXML
+    private Label period8;
+    @FXML
+    private Label period9;
+    @FXML
+    private Label period10;
+    @FXML
+    private Label period11;
+    @FXML
+    private Label period12;
+    @FXML
+    private Label day1;
+    @FXML
+    private Label day2;
+    @FXML
+    private Label day3;
+    @FXML
+    private Label day4;
+    @FXML
+    private Label day5;
+    @FXML
+    private Label day6;
+    @FXML
+    private Label day7;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+    }
+    
+}
